ae7278d4727dd31cfba9e6f8ce8d148feec78b9a
Santyew <134631502+Santyew@users.noreply.github.com>
2025-02-06 21:37:12 +0000

    Se agrega comentarios OperationsMap.java

--- crud-tarea/src/main/java/crud/santiago/operations/OperationsMap.java
+++ crud-tarea/src/main/java/crud/santiago/operations/OperationsMap.java
@@ -5,20 +5,37 @@ import java.util.Map;
 import crud.santiago.model.Empleado;
 
 public interface  OperationsMap {
-    
+
+    /**
+    * Crea un empleado
+    */
     boolean create(Empleado empleado);
-
+    /**
+    * Lee el identificador de empleado
+    */
     Empleado read(String identificador);
-
+    /**
+    * Lee el empleado 
+    */
     Empleado read(Empleado empleado);
-
+    /**
+    * Actualiza el empleado
+    */
     boolean update(Empleado empleado);
-
+    /**
+    * Elimina el empleado 
+    */
     boolean delete(Empleado empleado);
-
+    /**
+    * Elimina el identificador
+    */
     boolean delete(String identificador);
-
+    /**
+    * Crea un puesto y lo recorre por Map
+    */
     Map<String,Empleado> empleadosPorPuestoTreeMap(String puesto);
-
+    /**
+    * Crea una edad y lo recorre por Map
+    */
     Map<String,Empleado> empleadosPorEdadTreeMap(String fechaInicio, String fechaFin);
 }
