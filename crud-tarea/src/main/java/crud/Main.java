3f025943cdf52557099ec83d0fb2c61dff0b9e4b
Santyew <134631502+Santyew@users.noreply.github.com>
2025-02-06 21:39:04 +0000

    Se actualiza el Main

--- crud-tarea/src/main/java/crud/Main.java
+++ crud-tarea/src/main/java/crud/Main.java
@@ -1,7 +1,19 @@
 package crud;
 
+import crud.santiago.file.FileOperationsMap;
+import crud.santiago.model.Empleado;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FileOperations operaciones = new FileOperations();
+        Empleado empleado = new Empleado("3", "Sara", "Gerente", 15, "25/02/1995");
+        Empleado empleado1 = new Empleado("8", "Sergio", "Conserje", 85, "04/10/2004");
+        Empleado empleado2 = new Empleado("3", "Emmanuel", "Oficinista", 69, "06/08/1997");
+
+        operaciones.create(empleado);
+        operaciones.create(empleado1);
+        operaciones.update(empleado2);
+        System.out.println(operaciones.empleadosPorPuestoTreeMap("Gerente"));
+        System.out.println(operaciones.empleadosPorEdadTreeMap("06/08/1997", "04/10/2005"));
     }
 }
